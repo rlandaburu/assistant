64e75e342ad97169286de4fd0ca84a2ac5a8472f
rlandaburu <52452298+rlandaburu@users.noreply.github.com>
2025-03-07 08:11:39 -0300

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -1,12 +1,11 @@
 from openai import OpenAI
-import config
 import os
 
 
 
 def createAssistant(file_ids, title):
     #Create the OpenAI Client Instance
-    client = OpenAI(api_key=config.API_KEY)
+   client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
     #GET Instructions saved in the Settings.py File (We save the instructions there for easy access when modifying)
     instructions = """
@@ -40,7 +39,7 @@ def createAssistant(file_ids, title):
 
 def saveFileOpenAI(location):
     #Create OpenAI Client
-    client = OpenAI(api_key=config.API_KEY)
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
     #Send File to OpenAI
     file = client.files.create(file=open(location, "rb"),purpose='assistants')
@@ -57,7 +56,7 @@ def startAssistantThread(prompt,vector_id):
     #Initiate Messages
     messages = [{"role": "user", "content": prompt}]
     #Create the OpenAI Client
-    client = OpenAI(api_key=config.API_KEY)
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
     #Create the Thread
     tool_resources = {"file_search": {"vector_store_ids": [vector_id]}}
     thread = client.beta.threads.create(messages=messages,tool_resources=tool_resources)
@@ -68,21 +67,21 @@ def startAssistantThread(prompt,vector_id):
 
 def runAssistant(thread_id, assistant_id):
     #Create the OpenAI Client
-    client = OpenAI(api_key=config.API_KEY)
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
     run = client.beta.threads.runs.create(thread_id=thread_id,assistant_id=assistant_id)
     return run.id
 
 
 
 def checkRunStatus(thread_id, run_id):
-    client = OpenAI(api_key=config.API_KEY)
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
     run = client.beta.threads.runs.retrieve(thread_id=thread_id,run_id=run_id)
     return run.status
 
 
 
 def retrieveThread(thread_id):
-    client = OpenAI(api_key=config.API_KEY)
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
     thread_messages = client.beta.threads.messages.list(thread_id)
     list_messages = thread_messages.data
     thread_messages = []
@@ -96,5 +95,5 @@ def retrieveThread(thread_id):
 
 
 def addMessageToThread(thread_id, prompt):
-    client = OpenAI(api_key=config.API_KEY)
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
     thread_message = client.beta.threads.messages.create(thread_id,role="user",content=prompt)
